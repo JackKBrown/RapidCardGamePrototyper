25182edf34cc47748ea4b34376f94a8619922af5
d11jbrown <j.brown@double11.com>
2023-05-16 13:49:50 +0100

    adding the cardmaker

---
+++ CardMaker/CardMaker/CardDrawer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardMaker
+{
+    class CardDrawer
+    {
+        //
+        int CardWidth;
+        int CardHeight;
+        public CardDrawer()
+        {
+            //define necessary values such as size
+        }
+
+        public void Draw()
+        {
+            //get Layers
+            List<Layer> layers = new List<Layer>();
+            MergeLayers(layers);
+            return;
+        }
+
+        private void MergeLayers(List<Layer> layers)
+        {
+            //potential challenges tansparency not being included
+            var bitmap = new Bitmap(this.CardWidth, this.CardHeight);
+            using (var g = Graphics.FromImage(bitmap))
+{
+                foreach (var layer in layers)
+                {
+                    g.DrawImage(layer.Image, layer.X, layer.Y);
+                }
+            }
+        }
+    }
+
+    class Layer
+    {
+        //position of the layer on the final card
+        public int X;
+        public int Y;
+        public Image Image;
+        public Layer(int x, int y, string imagePath)
+        {
+            X = x;
+            Y = y;
+            Image = Bitmap.FromFile(imagePath);
+        }
+    }
+
+}
---
+++ CardMaker/CardMaker/RondlelonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardMaker
+{
+    class RondlelonParser
+    {
+        public static List<ActionCard> ParseActionCardCSV(string FilePath)
+        {
+            List<ActionCard> cards = new List<ActionCard>();
+            //parse the given file
+            return cards;
+        }
+    }
+}
